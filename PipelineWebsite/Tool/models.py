4b3fc2c483e24853f2c04c7bf44fabc14eab5905
Mohmamed Abuanda <m.abunada@msn.com>
2018-07-17 15:30:35 +0300

    Create first model

--- PipelineWebsite/Tool/models.py
+++ PipelineWebsite/Tool/models.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class FileModel(models.Model):
+    Email = models.EmailField()
+    File = models.FileField()
+    def __str__(self):
+        return str(self.Email)
